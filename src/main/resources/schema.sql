fdc9e6d31abc202c29f908d03cae4e74ce85ee47
devHudi <devhudi@gmail.com>
2022-05-31 00:09:23 +0900

    chore: schema.sql 추가

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS IMAGE;
+
+DROP TABLE IF EXISTS COMMENT;
+
+CREATE TABLE IMAGE
+(
+    id          bigint       not null auto_increment,
+    title       varchar(255) not null,
+    description varchar(255) not null,
+    image_url   text         not null,
+    created_at  timestamp default current_timestamp,
+    primary key (id)
+);
+
+CREATE TABLE COMMENT
+(
+    id         bigint       not null auto_increment,
+    author     varchar(255) not null,
+    body       varchar(255) not null,
+    created_at timestamp default current_timestamp,
+    primary key (id)
+);
